e14108b0a2a9b32ab84dcd2b0fc7478bebb13bd0
aepatino21 <aepatino.21@est.ucab.edu.ve>
2024-11-26 01:48:58 -0400

    Add another .csv and combined it with the others + fix id

--- datasets/concat_datasets.py
+++ datasets/concat_datasets.py
@@ -4,15 +4,18 @@ import pandas as pd
 data_frame = []
 
 # List of all of our .csv files (datasets)
-datasets = ['datasets/FotosIndigenas.csv', 'datasets/FotosIndigenas2.csv']
+datasets = ['datasets/FotosIndigenas.csv', 'datasets/FotosIndigenas2.csv', 'datasets/ImagenesIndigenasSalazar.csv']
 
 # Read and store all of our datasets in the data_frame list
 for csv_file in datasets:
-    data_chunk = pd.read_csv(csv_file)
+    data_chunk = pd.read_csv(csv_file, encoding='UTF-8')
     data_frame.append(data_chunk)
 
 # Combine all data from the .csv files
-combine_data = pd.concat(data_frame, ignore_index=True)
+combined_data = pd.concat(data_frame, ignore_index=True)
+
+# Asignar valores incrementales a la primera columna
+combined_data.iloc[:, 0] = range(1, len(combined_data) + 1)
 
 # Saved combined dataset
-combine_data.to_csv('datasets/CombinedFotosIndigenas.csv', index=False)
+combined_data.to_csv('datasets/CombinedFotosIndigenas.csv', index=False)
