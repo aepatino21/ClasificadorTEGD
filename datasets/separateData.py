3b25c2dd5500ed82f8584c2728ac918a913aeb7e
Ángel Patiño <aepatino.21@est.ucab.edu.ve>
2025-01-07 03:07:15 -0400

    Add a script to separate the data in 70% for training, 15% for testing & 15% for validation

---
+++ datasets/separateData.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+
+# Cargar el archivo CSV
+df = pd.read_csv('output.csv')
+
+# Separar aleatoriamente el DataFrame
+df = df.sample(frac=1).reset_index(drop=True)
+
+# Calcular los tamaños de los splits
+train_size = int(0.70 * len(df))
+val_size = int(0.15 * len(df))
+test_size = len(df) - train_size - val_size
+
+# Dividir el DataFrame en tres partes
+train_df = df[:train_size]
+val_df = df[train_size:train_size + val_size]
+test_df = df[train_size + val_size:]
+
+# Guardar cada parte en un nuevo archivo CSV
+train_df.to_csv('train.csv', index=False)
+val_df.to_csv('val.csv', index=False)
+test_df.to_csv('test.csv', index=False)
